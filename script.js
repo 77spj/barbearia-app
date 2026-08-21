9c5c80350a3aa492dc5482c16390362323ab4419
Jonathan Pereira <jonathanpsouza77@gmail.com>
2026-08-21 18:56:04 -0300

    Adiciona o botão pra exibir o formulário de agendamento

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoAgendamento = document.getElementById("btn-agendamento");
+
+const formularioAgendamento = document.getElementById("form-agendamento");
+
+botaoAgendamento.addEventListener("click", function() {
+    formularioAgendamento.style.display = "block";
+});
+
