de4639955914330bb033176bc237dba960ad48e8
Jonathan Pereira <jonathanpsouza77@gmail.com>
2026-08-22 00:21:00 -0300

    adiciona animação de clique nos botões da tela inicial

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const botaoAgendamento = document.getElementById("btn-agendamento");
-
 const formularioAgendamento = document.getElementById("form-agendamento");
+const botaoHorarios = document.getElementById("btn-horarios");
+const botaoVoltar = document.getElementById("btn-voltar");
 
 botaoAgendamento.addEventListener("click", function() {
-    formularioAgendamento.style.display = "block";
+    botaoAgendamento.classList.remove("ativo");
+    formularioAgendamento.classList.add("ativo");
+    botaoHorarios.classList.add("ativo");
+    botaoVoltar.classList.add("ativo");
+});
+
+botaoVoltar.addEventListener("click", function () {
+    botaoAgendamento.classList.add("ativo");
+    formularioAgendamento.classList.remove("ativo");
+    botaoHorarios.classList.remove("ativo");
+    botaoVoltar.classList.remove("ativo");
 });
 
